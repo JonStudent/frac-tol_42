4b223d0c8a669d2b68f1fc8d607343e62d734c73
xdant <estudante.jm@gmail.com>
2022-12-06 19:46:37 +0000

    readdy for 2 win

--- src/aux/mlx.c
+++ src/aux/mlx.c
@@ -12,14 +12,18 @@
 
 #include "../fractol.h"
 
+void	new_mlx_win(t_data *data, t_img *img)
+{	
+	img->img = mlx_new_image(data->mlx.mlx, WIDTH, HEIGHT);
+	img->addr = mlx_get_data_addr(img->img, \
+	&img->bits_per_pixel, &img->line_len, &img->endian);
+}
+
 void	init_mlx(t_data *data)
 {
-	data->mlx.mlx = mlx_init(); 
+	data->mlx.mlx = mlx_init();
 	data->mlx.win = mlx_new_window(data->mlx.mlx, WIDTH, HEIGHT, "Fract-ol");
-	data->mlx.img.img = mlx_new_image(data->mlx.mlx, WIDTH, HEIGHT);
-	data->mlx.img.addr = mlx_get_data_addr(data->mlx.img.img, &data->mlx.img.bits_per_pixel, \
-	&data->mlx.img.line_len, &data->mlx.img.endian);
-
+	new_mlx_win(data, &data->mlx.img);
 	mlx_key_hook(data->mlx.win, keyboard, &data);
 	mlx_mouse_hook(data->mlx.win, mouse, &data);
 }
