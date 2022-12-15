b45c38a90b0737b597b3c443fd1ef848613e9b2b
xdant <estudante.jm@gmail.com>
2022-12-15 09:28:56 +0000

    cant resize

--- src/win/mlx.c
+++ src/win/mlx.c
@@ -47,10 +47,6 @@ int	win_close(t_frtl *f)
 		handle_error(f, NULL);
 	return (0);
 }
-int	resize(t_frtl *f, int x, int y)
-{
-	printf("hi x: %dy: %d\n");
-}
 
 void	init_win(t_frtl *f)
 {
@@ -67,7 +63,6 @@ void	init_win(t_frtl *f)
 	mlx_key_hook(f->win, keyboard, f);
 	mlx_mouse_hook(f->win, mouse, f);
 	mlx_hook(f->win, 33, 1L << 5, win_close, f);
-	mlx_hook(f->win, 15, 1L << 18, win_close, f);
 	mlx_expose_hook(f->win, fill_win, f);
 }
 
